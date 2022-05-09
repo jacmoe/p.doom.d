b9910d4a5bee6a9aeddb7fc70abeb9d9e0f6af24
Jacob Moena <jacmoe.dk@gmail.com>
2022-05-09 19:00:40 +0200

    Removed Lua - it does not work

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +tng)      ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
@@ -148,7 +148,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       (lua +lsp)          ; one-based indices? one-based indices
+       ;; (lua +lsp)       ; one-based indices? one-based indices
        ;;markdown          ; writing docs for people to ignore
        ;; nim              ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
