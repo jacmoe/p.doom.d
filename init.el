ae0fa02989f88a42c29b8fc523801fd923ae4ae1
jacmoe <jacmoe.dk@gmail.com>
2023-03-11 00:28:23 +0100

    Haskell : Dante works much better than LSP with Stack

--- init.el
+++ init.el
@@ -114,8 +114,8 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        (cc +lsp
-        +cmake-mode
-        )           ; C > C++ == 1
+        +cmake-mode)
+                   ; C > C++ == 1
        clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -137,7 +137,7 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       (haskell +lsp)    ; a language that's lazier than I am
+       (haskell +dante)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
